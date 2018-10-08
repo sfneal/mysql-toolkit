6ab3f3010b1aeb12f8fa3d9e5fac6cb4d11c9b1d
mrstephenneal <stephen.neal14@gmail.com>
2018-10-08 11:18:36 -0400

    Moved Query and Results classes to new modules

---
+++ mysql/toolkit/query.py
@@ -0,0 +1,68 @@
+from mysql.connector import connect, Error
+from mysql.connector import errorcode
+
+
+class Query:
+    """
+    Query execution helper methods for the MySQL class.
+
+    Handles opening and closing a connection to a database source, fetching results
+    from a query, executing a query and batch executing multiple queries.
+    """
+    def __init__(self, config, enable_printing):
+        self.enable_printing = enable_printing
+        self._cursor = None
+        self._cnx = None
+        self._connect(config)
+
+    def _connect(self, config):
+        """Establish a connection with a MySQL database."""
+        print('\tMySQL connecting')
+        try:
+            self._cnx = connect(**config)
+            self._cursor = self._cnx.cursor()
+            self._printer('\tMySQL DB connection established')
+        except Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            raise err
+
+    def _printer(self, *msg):
+        """Printing method for internal use."""
+        if self.enable_printing:
+            print(*msg)
+
+    def _close(self):
+        """Close MySQL database connection."""
+        self._cursor.close()
+        self._cnx.close()
+
+    def _commit(self):
+        """Commit the changes made during the current connection."""
+        self._cnx.commit()
+
+    def _fetch(self, statement, _print=False):
+        """Execute a SQL query and return values."""
+        # Execute statement
+        self._cursor.execute(statement)
+        rows = []
+        for row in self._cursor:
+            if len(row) == 1:
+                rows.append(row[0])
+            else:
+                rows.append(list(row))
+        if _print:
+            self._printer('\tMySQL rows successfully queried', len(rows))
+
+        # Return a single item if the list only has one item
+        return rows[0] if len(rows) == 1 else rows
+
+    def execute(self, command):
+        self._cursor.execute(command)
+        self._commit()
+
+    def executemany(self, command):
+        self._cursor.executemany(command)
+        self._commit()
---
+++ mysql/toolkit/results.py
@@ -0,0 +1,39 @@
+class Results:
+    """
+    Result retrieval helper methods for the MySQL class.
+
+    Capable of fetching list of available tables/databases, the primary for a table,
+    primary key values for a table, number of rows in a table, number of rows of all
+    tables in a database.
+    """
+    def __init__(self):
+        pass
+
+    @property
+    def tables(self):
+        """Retrieve a list of tables in the connected database"""
+        statement = 'show tables'
+        return self._fetch(statement)
+
+    @property
+    def databases(self):
+        """Retrieve a list of databases that are accessible under the current connection"""
+        return self._fetch('show databases')
+
+    def get_primary_key(self, table):
+        """Retrieve the column which is the primary key for a table."""
+        for column in self.get_schema(table):
+            if 'pri' in column[3].lower():
+                return column[0]
+
+    def get_primary_key_values(self, table):
+        """Retrieve a list of primary key values in a table"""
+        return self.select(table, self.get_primary_key(table), _print=False)
+
+    def count_rows(self, table):
+        """Get the number of rows in a particular table"""
+        return self.select(table, 'COUNT(*)', False)
+
+    def count_rows_all(self):
+        """Get the number of rows for every table in the database."""
+        return {table: self.count_rows(table) for table in self.tables}
