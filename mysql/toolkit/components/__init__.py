2207465a61ec3ced1daca2ffe89ceeaedcb58ce2
mrstephenneal <stephen.neal14@gmail.com>
2018-10-08 11:47:53 -0400

    Created advanced module for advanced SQL queries

--- mysql/toolkit/components/__init__.py
+++ mysql/toolkit/components/__init__.py
@@ -1,6 +1,7 @@
 from mysql.toolkit.components.core import Core
-from mysql.toolkit.components.execute import ExecuteScript
 from mysql.toolkit.components.results import Results
+from mysql.toolkit.components.advanced import Advanced
+from mysql.toolkit.components.execute import ExecuteScript
 
 
-__all__ = ['Core', 'ExecuteScript', 'Results']
+__all__ = ['Core', 'Results', 'Advanced', 'ExecuteScript']
--- mysql/toolkit/toolkit.py
+++ mysql/toolkit/toolkit.py
@@ -1,13 +1,9 @@
-from differentiate import differentiate
 from mysql.connector import errors
-from mysql.toolkit.utils import wrap
 from mysql.toolkit.connector import Connector
-from mysql.toolkit.components.core import Core
-from mysql.toolkit.components.results import Results
-from mysql.toolkit.components.execute import ExecuteScript
+from mysql.toolkit.components import Core, Results, Advanced
 
 
-class MySQL(Connector, Core, Results):
+class MySQL(Connector, Core, Results, Advanced):
     def __init__(self, config, enable_printing=True):
         """
         Connect to MySQL database and execute queries
@@ -17,6 +13,7 @@ class MySQL(Connector, Core, Results):
         Connector.__init__(self, config, enable_printing)
         Core.__init__(self)
         Results.__init__(self)
+        Advanced.__init__(self)
 
     def __enter__(self):
         return self
@@ -29,97 +26,3 @@ class MySQL(Connector, Core, Results):
         except errors as e:
             print('\tError: ' + str(e))
             print('\tMySQL disconnected')
-
-    def truncate_database(self):
-        """Drop all tables in a database."""
-        # Get list of tables
-        tables = self.tables if isinstance(self.tables, list) else [self.tables]
-        if len(tables) > 0:
-            # Join list of tables into comma separated string
-            tables_str = ', '.join([wrap(table) for table in tables])
-            self.execute('DROP TABLE ' + tables_str)
-            self._printer('\t' + str(len(tables)), 'tables truncated')
-        return tables
-
-    def insert_uniques(self, table, columns, values):
-        """
-        Insert multiple rows into a table that do not already exist.
-
-        If the rows primary key already exists, the rows values will be updated.
-        If the rows primary key does not exists, a new row will be inserted
-        """
-        # Rows that exist in the table
-        existing_rows = self.select(table, columns)
-
-        # Rows that DO NOT exist in the table
-        unique = differentiate(existing_rows, values)  # Get values that are not in existing_rows
-
-        # Keys that exist in the table
-        keys = self.get_primary_key_values(table)
-
-        # Primary key's column index
-        pk_col = self.get_primary_key(table)
-        pk_index = columns.index(pk_col)
-
-        # Split list of unique rows into list of rows to update and rows to insert
-        to_insert, to_update = [], []
-        for index, row in enumerate(unique):
-            # Primary key is not in list of pk values, insert new row
-            if row[pk_index] not in keys:
-                to_insert.append(unique[index])
-
-            # Primary key exists, update row rather than insert
-            elif row[pk_index] in keys:
-                to_update.append(unique[index])
-
-        # Insert new rows
-        if len(to_insert) > 0:
-            self.insert_many(table, columns, to_insert)
-
-        # Update existing rows
-        if len(to_update) > 0:
-            self.update_many(table, columns, to_update, pk_col, 0)
-
-    def update_many(self, table, columns, values, where_col, where_index):
-        """Update the values of several rows."""
-        for row in values:
-            self.update(table, columns, row, (where_col, row[where_index]))
-
-    # def create_table(self, table, data, headers=None):
-    #     """Generate and execute a create table query by parsing a 2D dataset"""
-    #     # TODO: Fix
-    #     # Set headers list
-    #     if not headers:
-    #         headers = data[0]
-    #
-    #     # Create dictionary columns and data types from headers list
-    #     data_types = {header: None for header in headers}
-    #
-    #     # Confirm that each row of the dataset is the same length
-    #     for row in data:
-    #         assert len(row) == len(headers)
-    #
-    #     # Create list of columns
-    #     columns = [header + ' ' + data_type for header, data_type in data_types]
-    #     self._printer(columns)
-    #     statement = "create table " + table + " ("
-    #     self._printer(statement)
-
-    def drop_empty_tables(self):
-        """Drop all empty tables in a database."""
-        # Count number of rows in each table
-        counts = self.count_rows_all()
-        drops = []
-
-        # Loop through each table key and validate that rows count is not 0
-        for table, count in counts.items():
-            if count < 1:
-                # Drop table if it contains no rows
-                self.drop_table(table)
-                self._printer('Dropped table', table)
-                drops.append(table)
-        return drops
-
-    def execute_script(self, sql_script, commands=None):
-        """Wrapper method for ExecuteScript class."""
-        ExecuteScript(self, sql_script, commands)
