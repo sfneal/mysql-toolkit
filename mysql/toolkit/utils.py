6d203b39a9746057360e6934c65baad917ebe176
mrstephenneal <stephen.neal14@gmail.com>
2018-10-08 10:57:17 -0400

    Created utils module
    Helper functions for executing SQL queries.

---
+++ mysql/toolkit/utils.py
@@ -0,0 +1,29 @@
+def get_column_value_strings(columns, query_type='insert'):
+    cols = ""
+    vals = ""
+    if query_type == 'insert':
+        for c in columns:
+            cols = cols + c + ', '
+            vals = vals + '%s' + ', '
+
+        # Remove last comma and space
+        cols = cols[:-2]
+        vals = vals[:-2]
+        return cols, vals
+    if query_type == 'update':
+        for c in columns:
+            cols = str(cols + c + '=%s, ')
+
+        # Remove last comma and space
+        cols = cols[:-2]
+        return cols
+
+
+def join_columns(cols):
+    """Join list of columns into a string for a SQL query"""
+    return ", ".join([i for i in cols]) if isinstance(cols, list) else cols
+
+
+def wrap(item):
+    """Wrap a string with `` characters for SQL queries."""
+    return '`' + str(item) + '`'
