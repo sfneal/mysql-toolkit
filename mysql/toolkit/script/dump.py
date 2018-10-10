50a0cdf88a3ca4f7217da594ec4193f5c249b39b
mrstephenneal <stephen.neal14@gmail.com>
2018-10-10 13:37:17 -0400

    Added comment

--- mysql/toolkit/script/dump.py
+++ mysql/toolkit/script/dump.py
@@ -102,6 +102,8 @@ def write_read_commands(commands):
         _commands = pool.map(_write_read_packed, commands_packed)
         pool.close()
         print('\tRead and Wrote ', len(_commands), 'commands in', timer.end, '(multiprocessing)')
+
+        # Sort list by index and then return flat list of commands
         return [cmd_lst[0] for cmd_lst in sorted(_commands, key=lambda i: i[0])]
     else:
         return [_write_read(command) for command in tqdm(commands, total=len(commands),
