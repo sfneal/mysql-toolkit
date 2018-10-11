1a14544288ce2fc7179ea77e5a6e3676fe1d8954
mrstephenneal <stephen.neal14@gmail.com>
2018-10-11 10:08:52 -0400

    Added to test suite

---
+++ tests/test_prepare_2.py
@@ -0,0 +1,10 @@
+from mysql.toolkit.script.prepare import prepare_sql
+
+
+stmnt = '''
+
+'''
+
+print(stmnt)
+print('------------------------------------------------------------------------')
+print(prepare_sql(stmnt))
