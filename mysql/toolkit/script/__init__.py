93e380c044790e9a372387b92233366c5912ec96
mrstephenneal <stephen.neal14@gmail.com>
2018-10-11 09:09:10 -0400

    Refactored execute.py to script.py
    To signify that it is the main file of the script module.

--- mysql/toolkit/script/__init__.py
+++ mysql/toolkit/script/__init__.py
@@ -1,6 +1,6 @@
 from mysql.toolkit.script.split import SplitCommands
 from mysql.toolkit.script.dump import dump_commands
-from mysql.toolkit.script.execute import SQLScript
+from mysql.toolkit.script.script import SQLScript
 
 
 __all__ = ['SplitCommands', 'dump_commands', 'SQLScript']
