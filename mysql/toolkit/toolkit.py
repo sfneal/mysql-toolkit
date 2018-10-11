152f423a513a0707edebfa1bbdc6992132636c6f
mrstephenneal <stephen.neal14@gmail.com>
2018-10-11 10:43:22 -0400

    Modified __init__ method

--- mysql/toolkit/toolkit.py
+++ mysql/toolkit/toolkit.py
@@ -9,10 +9,11 @@ class MySQL(Connector, Core, Results, Advanced):
         :param config: MySQL server configuration settings
         """
         # Initialize inherited classes
-        Connector.__init__(self, config, enable_printing)
-        Core.__init__(self)
-        Results.__init__(self)
-        Advanced.__init__(self)
+        super().__init__(config, enable_printing)
+        # Connector.__init__(self, config, enable_printing)
+        # Core.__init__(self)
+        # Results.__init__(self)
+        # Advanced.__init__(self)
 
     def __enter__(self):
         return self
