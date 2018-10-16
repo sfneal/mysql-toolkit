1c00bdd3216a5bb70837354c78a88c49c8497c62
mrstephenneal <stephen.neal14@gmail.com>
2018-10-16 12:41:14 -0400

    Modified print statement in copy_database timer

--- mysql/toolkit/components/operations.py
+++ mysql/toolkit/components/operations.py
@@ -264,7 +264,7 @@ class Operations:
         -with-the-same-name-and-the-sa
         """
         print('\tCopying database {0} structure and data to database {1}'.format(source, destination))
-        with Timer('\tCopied database {0} to {1} in '.format(source, destination)):
+        with Timer('Success! Copied database {0} to {1} in '.format(source, destination)):
             # Create destination database if it does not exist
             if destination in self.databases:
                 self.truncate_database(destination)
