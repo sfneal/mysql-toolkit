0268fb855865d4595445a6758ebdcab35f045f9d
mrstephenneal <stephen.neal14@gmail.com>
2018-10-19 12:51:45 -0400

    Created Numeric and Text classes

--- mysql/toolkit/script/datatypes.py
+++ mysql/toolkit/script/datatypes.py
@@ -1,26 +1,31 @@
 DATA_TYPES = {
+    # Text Data Types
     'varchar': {'type': str, 'max': 255},
     'tinytext': {'type': str, 'max': 255},
     'text': {'type': str, 'max': 65535},
     'mediumtext': {'type': str, 'max': 16777215},
     'longtext': {'type': str, 'max': 4294967295},
+
+    # Numeric Data Types
+    'tinyint': {'type': int, 'min': -128, 'max': 127},
+    'smallint': {'type': int, 'min': -32768, 'max': 32767},
+    'mediumint': {'type': int, 'min': -8388608, 'max': 8388607},
+    'int': {'type': int, 'min': -2147483648, 'max': 2147483647},
+    'bigint': {'type': int, 'min': -2147483648, 'max': 2147483647},
+    'float': {'type': float},
 }
 
 
-class Record:
+class Text:
     def __init__(self, data):
         self.data = data
         self.type = None
-        self.max = len(self.data)
-
-    @property
-    def datatype(self):
-        return '{0} ({1})'.format(self.type, self.max)
+        self.len = len(self.data)
 
     def is_varchar(self):
         """Determine if a data record is of the type VARCHAR."""
         dt = DATA_TYPES['varchar']
-        if type(self.data) is dt['type'] and self.max < dt['max']:
+        if type(self.data) is dt['type'] and self.len < dt['max']:
             self.type = 'VARCHAR'
             return True
 
@@ -43,9 +48,60 @@ class Record:
     def _is_text_data(self, data_type):
         """Private method for testing text data types."""
         dt = DATA_TYPES[data_type]
-        if type(self.data) is dt['type'] and self.max < dt['max'] and all(type(char) == str for char in self.data):
+        if type(self.data) is dt['type'] and self.len < dt['max'] and all(type(char) == str for char in self.data):
             self.type = data_type.uppercase
             return True
+
+
+class Numeric:
+    def __init__(self, data):
+        self.data = data
+        self.type = None
+        self.len = len(self.data)
+
+    def is_tinyint(self):
+        """Determine if a data record is of the type TINYINT."""
+        return self._is_numeric_data('tinyint')
+
+    def is_mediumint(self):
+        """Determine if a data record is of the type MEDIUMINT."""
+        return self._is_numeric_data('mediumint')
+
+    def is_int(self):
+        """Determine if a data record is of the type INT."""
+        return self._is_numeric_data('mediumint')
+
+    def is_bigint(self):
+        """Determine if a data record is of the type BIGINT."""
+        return self._is_numeric_data('bigint')
+
+    def _is_numeric_data(self, data_type):
+        """Private method for testing text data types."""
+        dt = DATA_TYPES[data_type]
+        if type(self.data) is dt['type'] and dt['min'] < self.len < dt['max']:
+            self.type = data_type.uppercase
+            return True
+
+    def is_float(self):
+        """Determine if a data record is of the type float."""
+        dt = DATA_TYPES['float']
+        if type(self.data) is dt['type']:
+            self.type = 'FLOAT'
+            num_split = str(self.data).split('.', 1)
+            self.len = '{0}, {1}'.format(len(num_split[0]), len(num_split[1]))
+            return True
+
+
+class Record(Text, Numeric):
+    def __init__(self, data):
+        super(Record, self).__init__(data)
+        self.data = data
+        self.type = None
+        self.len = len(self.data)
+
+    @property
+    def datatype(self):
+        return '{0} ({1})'.format(self.type, self.len)
 
 
 class DataTypes:
