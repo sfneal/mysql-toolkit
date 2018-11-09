c545a3521a56939ec41eb87392edc24f7391f2da
mrstephenneal <stephen.neal14@gmail.com>
2018-11-09 09:29:35 -0500

    Added ORDER BY string inputting
    Modified _order_unpack function to handle strings with a column name and order preference (ASC, DESC).
    Added _format_order_clause function for unpacking column_order tuple, validating order and returning formatted string.

--- mysql/toolkit/components/manipulate/_order.py
+++ mysql/toolkit/components/manipulate/_order.py
@@ -1,8 +1,33 @@
+ORDER_CLAUSES = ('asc', 'desc')
+
+
+def _format_order_clause(column_order):
+    """Unpack (column, asc_desc) tuple and validate formatting."""
+    column, asc_desc = column_order
+    assert asc_desc.lower() in ORDER_CLAUSES, "Invalid ORDER BY clause, must be either 'ASC' or 'DESC'"
+    return '{0} {1}'.format(column, asc_desc.upper())
+
+
 def _order_unpack(order_by):
-    """Unpack ascending/descending clause from order_by clause."""
-    if isinstance(order_by, tuple):
-        column, asc_desc = order_by
-        return '{0} {1}'.format(column, asc_desc.upper())
+    """
+    Unpack ascending/descending clause from order_by clause.
+
+    Try and unpack a (column, asc_desc) tuple.  Then try and find ' asc' or ' desc' in order_by,
+    checking for leading whitespace to avoid false matches for column names containing a 'asc'
+    or 'desc' string.  If first two checks do not pass, order_by is returned as inputted.
+    :param order_by: String or Tuple containing a column name and optional ascending or descending
+    :return: Formatted ORDER BY clause
+    """
+    # Unpack (column, asc_desc) tuple
+    if isinstance(order_by, tuple) and len(order_by) == 2:
+        return _format_order_clause(order_by)
+
+    # Found asc_desc string with leading whitespace
+    elif isinstance(order_by, str) and any(' {0}'.format(clause) in order_by for clause in ORDER_CLAUSES):
+        # Split order by string once
+        return _format_order_clause(tuple(order_by.split(' ', 1)))
+
+    # Return original input
     else:
         return order_by
 
